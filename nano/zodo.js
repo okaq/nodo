e6a8684c389c75f9e79192411a7064ec6c2657f6
AQ <aq@okaq.com>
2020-07-22 09:59:44 -0400

    Main function initialize and begin loop

--- nano/zodo.js
+++ nano/zodo.js
@@ -1,4 +1,25 @@
 // live msg
 console.log("start zodo");
 
+// scene
+const sce = {
+    init() {
+        sce.a = {};
+    }
+};
 
+// loop
+const loop = {
+    start() {
+        console.log("begin loop");
+        loop.tick = 0;
+    }
+};
+
+// main
+(function() {
+    console.log(THREE);
+    subs = [dom,sce];
+    subs.forEach(el => el.init());
+    loop.start();
+})();
