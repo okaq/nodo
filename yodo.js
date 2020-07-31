b1970f3acf6fa8d09921bebcffa2a0d15f18517c
okaq <aq@okaq.com>
2020-07-31 09:17:49 -0400

    Scene and geometry init

--- yodo.js
+++ yodo.js
@@ -1,6 +1,22 @@
 // nodo
 console.log("hello nodo");
 
+// scene
+const sce = {
+    init() {
+        sce.a = {};
+        sce.a.r = [1920,1080,0,0];
+        sce.a.c = dom.canvas(sce.a.r);
+        dom.add(sce.a.c);
+    }
+};
+
+// geometry
+const geo = {
+    init() {
+        geo.a = {};
+    }
+};
 
 // DOM
 const dom = {
