96577e342999b73477a9e9cda8b65debde6c9482
okaq <aq@okaq.com>
2020-07-31 07:48:04 -0400

    DOM init

--- yodo.js
+++ yodo.js
@@ -1,3 +1,45 @@
 // nodo
 console.log("hello nodo");
 
+
+// DOM
+const dom = {
+	init() {
+		dom.a = document.getElementById("alpha");
+		console.log(dom.a);
+	},
+	canvas(r0) {
+		let c0 = {};
+
+		c0.can = document.createElement("canvas");
+		c0.can.width = r0[0];
+		c0.can.height = r0[1];
+		c0.can.style.position = "absolute";
+		c0.can.style.top = r0[3] + "px";
+		c0.can.style.left = r0[2] + "px";
+
+		return c0;
+	},
+	add(c0) {
+		dom.a.appendChild(c0.can);
+	},
+	remove(c0) {
+		dom.a.appendChild(c0.can);
+	}
+};
+
+// color
+const rgba = {
+	rb() {
+		return (Math.random() * 255) >>> 0;
+	}
+};
+
+// main
+(function() {
+    console.log(THREE);
+    subs = [dom,sce,geo];
+    subs.forEach(el => el.init());
+    loop.start();
+})();
+
